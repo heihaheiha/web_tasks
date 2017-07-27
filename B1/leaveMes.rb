309d9a61e15937ef064d49754d2f3979fdc9fb85
heihaheiha <1217553596@qq.com>
2017-07-27 19:34:47 +0800

    有修改后的版本

--- B1/leaveMes.rb
+++ B1/leaveMes.rb
@@ -7,78 +7,11 @@ require 'yaml'
 $num = 0
 $arr = []
 get '/' do
-  #erb :inquery
-  #id_t = params[:find_id].to_s
-  #author_t = params[:find_author].to_s
-  arrStore = []
-  if File::exists?('storeLeaveMessage.yml')
-  	con = YAML.load(File.open('storeLeaveMessage.yml'))
-  	con.each do |p|
-  	  arrStore << p
-  	end
-  else 
-  	language = "you haven't downloaded the file called storeLeaveMessage.yml"
-  	erb :error
-  	break
-  end
-    $arr = arrStore
-    puts $arr[0]
-    puts $arr[0]["id"]
-  erb :seeMess
-  #   len = arrStore.length
-  # if id_t.empty? and author_t.empty?
-  # 	puts arrStore[0]
-  # 	$arr.clear
-  #   $arr = arrStore
-  #   puts $arr
-  #   puts $arr[0]
-  #   puts $arr[0]["id"]
-  # 	erb :seeMess
-  # 	break
-  # elsif id_t.empty?
-  # 	#find author
-  # 	i = 0
-  # 	puts "enter id_t.empty?)"
-  # 	$arr.clear
-  # 	len.times{
-  # 	  if(arrStore[i]["author"].eql?(author_t))
-  # 		$arr << arrStore[i]
-  # 	  else i += 1
-  # 	  end
-  # 	}
-  # 	erb :seeMess
-  # 	break
-  # elsif author_t.empty?
-  # 	#find id
-  # 	i = 0
-  # 	puts "enter author_t.empty?"
-  # 	$arr.clear
-  # 	len.times{
-  # 	  if(arrStore[i]["id"].eql?(id_t))
-  # 		$arr << arrStore[i]
-  # 		break
-  # 	  else i += 1
-  # 	  end
-  # 	}
-  # 	#judge whether it has the id
-  # 	if $arr.length == 1
-  # 	  erb :seeMess
-  # 	else 
-  # 	  language = "the id isn't existed"
-  # 	  erb :error
-  # 	end
-  # 	break
-  # else
-  # 	puts "enter the else"
-  #   language = "You have inputed two options"
-  #   erb :error
-  #   break
-  # end
+  erb :inquery
 end
 
+#enter the show data page
 get '/posts' do
-  id_t = ""
-  author_t = ""
   id_t = params[:find_id].to_s
   author_t = params[:find_author].to_s
   arrStore = []
@@ -87,65 +20,70 @@ get '/posts' do
   	con.each do |p|
   	  arrStore << p
   	end
+    len = arrStore.length
+    if len == 0
+      @language = "no content"
+      erb :error
+      break
+    #judge if it hasn't parameters,if it hasn't parameters,it show all the data
+    elsif id_t.empty? and author_t.empty?
+    $arr = arrStore
+    erb :seeMess
+    #show the specified author data
+    elsif id_t.empty?
+    #find author
+    i = 0
+    puts "enter id_t.empty?"
+    puts author_t
+    $arr.clear
+    len.times{
+      puts arrStore[i]["author"]
+      if author_t.eql?(arrStore[i]["author"].to_s)
+        puts arrStore[i]["author"]
+        $arr << arrStore[i]
+        i += 1
+      else i += 1
+      end
+    }
+    puts $arr
+    if $arr.length == 0
+      @language = "this file hasn't haved the author"
+      erb :error
+    else
+      erb :seeMess
+    end
+    #show the specified id data
+    elsif author_t.empty?
+    #find id
+    i = 0
+    #puts "enter author_t.empty?"
+    #puts len
+    $arr.clear
+    len.times{
+      if arrStore[i]["id"].to_s.eql?(id_t)
+        $arr << arrStore[i]
+        break
+      else i += 1
+      end
+    }
+    #judge whether it has the id
+    if i == len
+      @language = "the id isn't existed"
+      erb :error
+    else erb :seeMess
+    end
+    #when it transfers two parameters,it will show the error data
+    else
+      puts "enter the else"
+      @language = "You have inputed two options"
+      erb :error
+    end
+  #when the file doesn't existed,it will show the error page
   else 
-  	language = "you haven't downloaded the file called storeLeaveMessage.yml"
+    puts "1"
+  	@language = "you haven't downloaded the file called storeLeaveMessage.yml"
   	erb :error
-  	break
   end
-
-  $arr = arrStore
-    puts $arr[0]
-    puts $arr[0]["id"]
-  erb :seeMess
-  len = arrStore.length
-  # if id_t.empty? and author_t.empty?
-  # 	puts arrStore[0]
-  #   $arr = arrStore
-  #   puts $arr
-  #   puts $arr[0]
-  #   puts $arr[0]["id"]
-  # 	erb :seeMess
-  # 	break
-  # elsif id_t.empty?
-  # 	#find author
-  # 	i = 0
-  # 	puts "enter id_t.empty?)"
-  # 	$arr.clear
-  # 	len.times{
-  # 	  if(arrStore[i]["author"].eql?(author_t))
-  # 		$arr << arrStore[i]
-  # 	  else i += 1
-  # 	  end
-  # 	}
-  # 	erb :seeMess
-  # 	break
-  # elsif author_t.empty?
-  # 	#find id
-  # 	i = 0
-  # 	puts "enter author_t.empty?"
-  # 	$arr.clear
-  # 	len.times{
-  # 	  if(arrStore[i]["id"].eql?(id_t))
-  # 		$arr << arrStore[i]
-  # 		break
-  # 	  else i += 1
-  # 	  end
-  # 	}
-  # 	#judge whether it has the id
-  # 	if $arr.length == 1
-  # 	  erb :seeMess
-  # 	else 
-  # 	  language = "the id isn't existed"
-  # 	  erb :error
-  # 	end
-  # 	break
-  # else
-  # 	puts "enter the else"
-  #   language = "You have inputed two options"
-  #   erb :error
-  #   break
-  # end
-  	
 end
 #enter the leave message page
 get '/add' do
@@ -153,32 +91,38 @@ get '/add' do
 end
 #尝试删除对应ID的留言，向用户返回操作结果
 get '/delete/:id' do
-  id_s = params[:id]
+  id_s = params[:id].to_s
+  puts id_s
   if id_s.empty?
-  	language = "the length of id is zero"
+  	@language = "the length of id is zero"
   	erb :error
-  end
-  arrStore = []
-  con = YAML.load(File.open('storeLeaveMessage.yml'))
-  con.each do |p|
-    arrStore << p
-  end
-  len = arrStore.length
-  i = 0
-  len.times{
-  	if(arrStore[i]["id"].eql?(id_s))
-  	  arrStore.delete_at(i)
-  	  File.open('storeLeaveMessage.yml', 'w') do |os|
-  	    YAML::dump(arrStore, os)
+  else
+    puts "enter"
+    arrStore = []
+    con = YAML.load(File.open('storeLeaveMessage.yml'))
+    con.each do |p|
+      arrStore << p
+    end
+    len = arrStore.length
+    puts len
+    i = 0
+    #judge whether the id is existed
+    len.times{
+      #puts i
+      if arrStore[i]['id'].to_s.eql?(id_s)
+        arrStore.delete_at(i)
+        File.open('storeLeaveMessage.yml', 'w') do |os|
+          YAML::dump(arrStore, os)
+        end
+        redirect '/posts?find_id=&find_author='
+        break
+      else i += 1
       end
-  	  redirect '/'
-   	  break
-   	else i += 1
-  	end
-  }
-  if i == len 
-  	language = "id isn't existed in the file"
-  	erb :error
+    }
+    if i == len 
+      @language = "id isn't existed in the file"
+      erb :error
+    end
   end
 end
 #form表单传送message和author信息，收到前先验证数据，保证内容不为空，然后插
@@ -204,14 +148,12 @@ post '/add' do
     $num = $num+1
     id_f = $num  
   end
-  $num = $num+1
-  id_f = $num
   writer_f = params[:awriter].to_s
   leaveMes_f = params[:content].to_s
   time_f = Time.new
   puts writer_f
   puts leaveMes_f
-  arr1 = []
+  #arr1 = []
   if leaveMes_f.length <= 10
   	@language = "the length of content is less than 10"
   	erb :error
@@ -221,11 +163,14 @@ post '/add' do
   else
   	message = LeMess.new(id_f, writer_f.chomp, leaveMes_f.chomp, time_f.strftime("%Y-%m-%d %H:%M:%S"))
     mess1 = Hash["id" => message.id, "author" => message.author, "content" => message.content, "time" => message.time]
-    arr1 << mess1
-    puts arr1[0]['id']
-    File.open('storeLeaveMessage.yml', 'a+') do |os|
-  	  YAML::dump(arr1, os)
+    con = YAML.load(File.open('storeLeaveMessage.yml'))
+    con.each do |p|
+      arrStore << p
     end
+      arrStore << mess1
+      File.open('storeLeaveMessage.yml', 'w') do |os|
+        YAML::dump(arrStore, os)
+      end
     redirect '/'
   end
 end
